c473afa381aa90dd99188412cb8cec6d2a67bbc6
sshekhar3 <Sunny_Shekhar@intuit.com>
2024-10-23 19:04:28 +0530

    06. Hardcode ratings in Catalog Service

--- src/main/java/io/tribe/movie_info_service/resources/MovieResource.java
+++ src/main/java/io/tribe/movie_info_service/resources/MovieResource.java
@@ -6,17 +6,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.List;
-
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
     
     @GetMapping("/{movieId}")
-    public List<Movie> getMovieInfo(@PathVariable("movieId") String movieId ){
-        return Collections.singletonList(
-                new Movie("1234","John Wick")
-        );
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId ){
+        return new Movie(movieId,"Name for ID "+ movieId);
     }
 }
